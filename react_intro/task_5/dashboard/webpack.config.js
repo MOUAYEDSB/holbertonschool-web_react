31d420b1a94c4d119c99fd3ebf1529d060faee91
MOUAYEDSB <sabbaghmouayed@gmail.com>
2024-04-24 20:10:36 +0100

    Create a project using Webpack

---
+++ react_intro/task_5/dashboard/webpack.config.js
@@ -0,0 +1,42 @@
+// webpack.config.js
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: './dist',
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './dist/index.html', // Update the template path
+        }),
+    ],
+    devtool: 'inline-source-map',
+};
